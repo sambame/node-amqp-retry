52f40d2597f9b50511f2099e3904bf2d362535d0
Shay Erlichmen <erlichmen@gmail.com>
2014-01-19 17:02:45 +0200

    Use same semantics as the AppEngine

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
-module.exports = function wrapper(initialWait, maxTry, cb) {
-  return function(message, header, deliveryInfo, job) {
-    header.retries = header.retries || 0;
-    job.retry = function(wait, tries) {
-      if( wait === undefined ) {
-        wait = initialWait;
-      }
-      if(tries === undefined) {
-        tries = maxTry;
-      }
-      var delay = wait instanceof Array ? wait[header.retries] : Math.pow(2, header.retries)*wait;
-      header.retries++;
-      if(header.retries >= tries) {
-        return cb(new Error('Message processing failed ' + maxTry + ' times'), message, header, deliveryInfo, job);
-      }
-      var messageOptions = Object.keys(deliveryInfo).reduce(function(obj, key) {
-        obj[key] = deliveryInfo[key];
-        return obj;
-      }, {});
-      messageOptions.headers = header;
-      require('amqp-schedule')(job.queue.connection)(deliveryInfo.exchange, deliveryInfo.routingKey, message, delay, messageOptions);
+/*jslint node: true */
+"use strict";
+
+var amqpSchedule = require('amqp-schedule'),
+    util = require('util'),
+    defaultMinBackoffSeconds = 0.1,
+    dfaultMaxDoublings = 19;
+
+module.exports = function wrapper(parameters, cb) {
+    return function (message, header, deliveryInfo, job) {
+        header.retries = header.retries || 0;
+
+        job.retry = function () {
+            var taskRetryLimit = parameters.taskRetryLimit,
+                minBackoffSeconds = parameters.minBackoffSeconds || defaultMinBackoffSeconds,
+                maxBackoffSeconds = parameters.maxBackoffSeconds,
+                maxDoublings = parameters.maxDoublings;
+
+            var delay = Math.pow(2, Math.min(maxDoublings || header.retries, header.retries)) * minBackoffSeconds;
+
+            if (minBackoffSeconds && minBackoffSeconds > delay) {
+                delay = minBackoffSeconds;
+            }
+
+            if (maxBackoffSeconds && maxBackoffSeconds < delay) {
+                delay = maxBackoffSeconds;
+            }
+
+            if (taskRetryLimit && header.retries >= taskRetryLimit) {
+                // TODO some sort of logging
+                return message.reject(false);
+            }
+
+            header.retries++;
+
+            var messageOptions = Object.keys(deliveryInfo).reduce(function (obj, key) {
+                obj[key] = deliveryInfo[key];
+                return obj;
+            }, {});
+
+            messageOptions.headers = header;
+            console.log('task %s with delay %s', JSON.stringify(message), delay)
+            amqpSchedule(job.queue.connection)(deliveryInfo.exchange, deliveryInfo.routingKey, message, delay * 1000, messageOptions);
+        };
+
+        cb(message, header, deliveryInfo, job);
     };
-    try {
-      cb(null, message, header, deliveryInfo, job);
-    } catch(e) { console.error(e); }
-  };
 };
